e15723b4f3c3a6b68d2e468fb7406f13f293df23
dmcbrayer <dmcbrayer@gmail.com>
2015-01-15 18:32:26 -0500

    Initial Commit

---
+++ parse_emails.rb
@@ -0,0 +1,17 @@
+require 'json'
+require 'csv'
+
+#read JSON from a file
+file = File.open("_User.json")
+json = file.read
+
+#parse that JSON with the official JSON parser
+parsed = JSON.parse(json)
+
+
+#for everything in "results", put the content of each "user" field into an array
+CSV.open('emails.csv', 'w') do |csv|
+	parsed["results"].each do |user|
+		csv << [user["email"]]
+	end
+end
