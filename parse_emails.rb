520ed81e2921a73629ce2bfc7be32ec8668a47d4
dmcbrayer <dmcbrayer@gmail.com>
2015-03-10 07:22:37 +0800

    Added arbitrary date filter

--- parse_emails.rb
+++ parse_emails.rb
@@ -1,5 +1,6 @@
 require 'json'
 require 'csv'
+require 'date'
 
 #Open _User.json and run it through the JSON parser
 #and save the results as an array
@@ -9,6 +10,9 @@ parsed = JSON.parse(File.open("_User.json").read)
 #the contents of each user's "email" attribute into a csv row.
 CSV.open('emails.csv', 'w') do |csv|
   parsed["results"].each do |user|
-    csv << [user["email"]]
+  	created_date = user["createdAt"][0...-14]
+  	if created_date > '2015-03-01'
+    	csv << [user["email"], created_date]
+    end
   end
 end
